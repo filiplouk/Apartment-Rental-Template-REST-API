2883f8391df6aca453d53db8e8e5eaed2fbd9ad7
filiplouk <94610898+filiplouk@users.noreply.github.com>
2023-01-04 19:22:03 +0200

    Add Cors Configuration

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = reqquire("cors");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const app = express();
@@ -10,6 +11,8 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use(cors());
+
 
 
 // Mongoose 
