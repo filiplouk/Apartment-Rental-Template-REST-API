c09cf60a52d120a9006da67967d4519a0441fb7d
filiplouk <94610898+filiplouk@users.noreply.github.com>
2023-01-16 09:12:54 +0200

    Replace Callbacks with async await/ Try catch instead of if/ Make get routes receive and send chunks of data and not whole data/Beautify code/Replace code with less code

--- app.js
+++ app.js
@@ -1,169 +1,127 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const errorHandler = require("./middleware/errorHandler");
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(cors());
 
-
-
-// Mongoose 
+// Mongoose
 const mongo_password = process.env.MONGO_PASSWORD;
-mongoose.connect('mongodb+srv://admin-filip:'+mongo_password+'@cluster0.kl1kndo.mongodb.net/propertyDB', {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  "mongodb+srv://admin-filip:" +
+    mongo_password +
+    "@cluster0.kl1kndo.mongodb.net/propertyDB",
+  {
+    useNewUrlParser: true,
+  }
+);
 
 const propertySchema = new mongoose.Schema({
-  name:{
-    type:String,
-    required:true
-    
+  name: {
+    type: String,
+    required: { value: true, message: "Please add property name" },
   },
-  units:{
-    type:[String],
-    required:true,
-    validate:{
-      validator: v => {
-        if (v==="undefined"){
-          return false;
-        }
-        let allowed_values = ["kitchen", "bedroom", "bathroom", "living-room"];
-        let flag=true;
-        v.forEach(value=>{
-          if (!allowed_values.includes(value)){
-            flag=false;
-          }
-        })
-        return flag
-      },
-      message:"Please add rooms from the list"
-    }
-  }
+  units: {
+    type: [String],
+    required: true,
+    enum: {
+      values: ["kitchen", "bedroom", "bathroom", "living-room"],
+      message: "Please add rooms from the list",
+    },
+  },
 });
 
 const Property = mongoose.model("Property", propertySchema);
 
-
-//
-
-
-
-
-// class Properties {
-//   constructor(){
-//     this.properties = {properties:[]};
-//   }
-
-//   newProperty(property){
-    
-//   }
-
-//   getAllProperties(){
-//     return(this.properties);
-//   }
-
-//   getProperty(name){
-
-//   }
-
-//   deleteProperty(name){
-//     let updatedProperties = this.properties.filter(each => each.name !== name);
-//     this.properties = updatedProperties;
-//   }
-// }
-
-
-
-
-app.get("/properties", (req,res)=>{
-  
-  Property.find({}).sort({name:1}).exec(function(err,data){
-    if (err){
-      res.send(err)
+app.get("/properties", async (req, res, next) => {
+  try {
+    let count = req.query.count;
+    let cursor = await Property.find({})
+      .sort({ name: 1 })
+      .skip(count)
+      .limit(5)
+      .cursor();
+    let data = [];
+    let doc = await cursor.next();
+    while (doc) {
+      data.push(doc);
+      doc = await cursor.next();
     }
-    else{
-      console.log("data sent")
-      res.json({
-        properties:data
-      });
-    }
-  })
-})
-
-app.post("/properties", (req,res)=>{
-   const newProperty = new Property(req.body);
-   
-   newProperty.save(function(err){
-    if (err){
-      console.log(err);
-      res.send(err.message);
-    }
-    else{
-      console.log("new property added")
-      res.send("Congratulations! Your property has been uploaded.")
-    }
-   })
-
-})
-
-app.delete("/properties", (req,res)=>{
-  if (req.body.name===""){
-    res.send("Please enter a property name.")
+    res.status(200).json({ properties: data });
+  } catch (err) {
+    next(err);
   }
-  else{
-    Property.findOneAndDelete({name:req.body.name}, function(err, value){
-      if (err){
-        res.send(err);
-      }
-      else{
-        if (value===null){
-          res.send("Property not found.")
-        }
-        else{
-          res.send('Property successfully deleted!')
-        }
-      }
-    })
+});
+
+app.post("/properties", async (req, res, next) => {
+  try {
+    const newProperty = new Property(req.body);
+    if (newProperty.units.length === 0) {
+      return res.send("Please add rooms.");
+    }
+    await newProperty.save();
+    console.log("new property added");
+    res.status(200).send("Congratulations! Your property has been uploaded.");
+  } catch (err) {
+    return next(err);
   }
-})
+});
 
-app.get("/properties/:bedrooms",(req,res)=>{
-  const number_of_bedrooms = Number(req.params.bedrooms.slice(-1));
- 
-  Property.find({}, (err,docs)=>{
-    let data=[];
-    docs.forEach(element=>{
-      if (count(element.units, "bedroom")===number_of_bedrooms){
-        console.log(element)
-        data.push(element)
+app.delete("/properties", async (req, res, next) => {
+  try {
+    if (req.body.name === "") {
+      return res.send("Please enter a property name.");
+    }
+    const value = await Property.findOneAndDelete({ name: req.body.name });
+    if (value === null) {
+      return res.send("Property not found.");
+    }
+    res.status(200).send("Property successfully deleted!");
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.get("/properties/:bedrooms", async (req, res, next) => {
+  try {
+    const number_of_bedrooms = Number(req.params.bedrooms.slice(-1));
+    const docs = await Property.find({});
+    let data = [];
+    docs.forEach((element) => {
+      if (count(element.units, "bedroom") === number_of_bedrooms) {
+        console.log(element);
+        data.push(element);
       }
-    })
-    res.json(data)
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    return next(err);
+  }
 
-  })
-  function count(array, element){
+  function count(array, element) {
     let count = 0;
-    array.forEach(item => {
-      if (item === element){
-        count +=1;
+    array.forEach((item) => {
+      if (item === element) {
+        count += 1;
       }
-    })
-    return(count)
+    });
+    return count;
   }
-  
-  
+});
 
-})
+app.use(errorHandler);
 
-
-app.listen(5000, function() {
+app.listen(5000, function () {
   console.log("Server started on port 5000");
 });
